d72b0f27900c92c3ef8cff9c341f22072c53c886
himerian <geazeilort@yandex.ru>
2025-10-03 21:37:46 +0400

    mobile1

--- source/conf.py
+++ source/conf.py
@@ -71,7 +71,7 @@ exclude_patterns = []
 #    furo
 #    pydata_sphinx_theme
 
-html_theme = 'agogo'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
