d44eac623bdf99d21ab4eed71fb8cbecb3a44b79
himerian <97090382+himerian@users.noreply.github.com>
2025-11-12 08:44:23 +0400

    Update conf.py
    turn off nav

--- source/conf.py
+++ source/conf.py
@@ -73,6 +73,11 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 
+html_sidebars = {
+    '**': []
+}
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
